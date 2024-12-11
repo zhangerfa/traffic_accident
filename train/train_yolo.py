cb8391882b3088fa26d8a18aa0e603453f57e750
erfa <877319363@qq.com>
2024-12-11 20:42:21 +0800

    将训练相关代码移动到train目录下

--- train/train_yolo.py
+++ train/train_yolo.py
@@ -12,4 +12,4 @@ if __name__ == '__main__':
     # batch是每次迭代的数据量，也就是一次加载的数据个数，数值越大训练速度越快，数值越小训练速度越慢，受限于显存大小
     # imgsz是图片的大小，如果输入图片大小不一致，yolo内部会自动调整大小，imgsz越大训练速度越慢，但数值小可能影响精度，因为图片压缩会导致信息丢失
     # workers是数据加载的线程数，受限于cpu核心数，数值越大训练速度越快，数值越小训练速度越慢
-    model.train(data=r'data/type.yaml', epochs=300, batch=64, imgsz=416, workers=4)
+    model.train(data=r'train/type.yaml', epochs=300, batch=64, imgsz=416, workers=4)
