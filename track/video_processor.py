efb2e88cf5b75be669d161b9c3fb1bc9b0cdcaf9
erfa <877319363@qq.com>
2025-02-05 17:46:57 +0800

    将轨迹的保存和提取移动到video_processor中

--- track/video_processor.py
+++ track/video_processor.py
@@ -1,6 +1,7 @@
 """
 集成视频识别、追踪模型，轨迹处理对象
 """
+import csv
 import datetime
 import logging
 
@@ -10,8 +11,7 @@ from track.car import Car
 from track.obb_tracker import ObbTracker
 from utils.draw_utils import draw_box, gene_colors, draw_trace_on_frame, draw_speed_cluster
 from utils.predict_utils import predict_and_show_frame
-from utils.trace_utils import cal_lane_from_trace, extract_trace_to_csv, load_trace_from_csv, \
-    extract_speed_ls_from_cars, cluster_speed_vector
+from utils.trace_utils import cal_lane_from_trace, extract_speed_ls_from_cars, cluster_speed_vector
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -40,16 +40,65 @@ class VideoProcessor:
         self.__extract_trace(frame_index, class_id_id)
         return cal_lane_from_trace(list(self.car_dict.values()), frame_index, time_span)
 
-    def extract_trace_to_csv(self, output_path, frame_count=None, class_id_id=None):
+    def extract_trace_to_csv(self, output, frame_count=None, class_id_id=None):
         """
-        从视频中提取轨迹并保存为csv文件
+        从视频中提取轨迹并保存到csv文件
+        cars：car实例对象列表
+        frame_count: 提取轨迹的帧数，如frame_count=200，就是提取前200帧的轨迹
         """
         self.__extract_trace(frame_count, class_id_id)
-        extract_trace_to_csv(list(self.car_dict.values()), output_path, self.trace_frame_count)
+        cars = list(self.car_dict.values())
+
+        fieldnames = ['obj_id', 'trace_time', 'class_id', 'x', 'y', 'w', 'h', 'angle']
+
+        with open(output, "w", newline='') as f:
+            # 1. 插入元数据
+            f.write(f'# frame_count: {frame_count}\n')
+            # 2. 插入轨迹数据
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            # 写入列名（表头）
+            writer.writeheader()
+            # 写入数据
+            for car in cars:
+                for trace in car.trace_ls:
+                    box = trace[0]
+                    row = {
+                        'obj_id': car.id,
+                        'trace_time': trace[1],
+                        'class_id': car.class_id,
+                        'x': box[0],
+                        'y': box[1],
+                        'w': box[2],
+                        'h': box[3],
+                        'angle': box[4]
+                    }
+                    writer.writerow(row)
+            logger.info(f"轨迹已保存到{output}")
+
 
     def load_trace_from_csv(self, csv_file_path):
-        # 从csv文件中加载轨迹数据，其中csv文件中的元数据为轨迹提取范围：trace_frame_count
-        self.car_dict, self.trace_frame_count = load_trace_from_csv(csv_file_path)
+        """
+        从csv文件中加载轨迹数据
+        其中csv文件中的元数据为已提取轨迹的帧范围：trace_frame_count，如frame_count=200表示已提取前200帧的轨迹
+        """
+        with open(csv_file_path, 'r') as f:
+            # 读取元数据
+            metadata = f.readline().strip()
+            if metadata.startswith("# frame_count:"):
+                self.trace_frame_count = int(metadata.split(":")[-1])
+            else:
+                logger.error("Error: 未找到轨迹数据的元数据{trace_frame_count}")
+            reader = csv.DictReader(f)
+            for row in reader:
+                obj_id = int(row['obj_id'])
+                trace_time = int(row['trace_time'])
+                class_id = row['class_id']
+                box = [float(row['x']), float(row['y']), float(row['w']), float(row['h']), float(row['angle'])]
+
+                if obj_id not in self.car_dict:
+                    self.car_dict[obj_id] = Car(obj_id, class_id)
+                self.car_dict[obj_id].trace_ls.append([box, trace_time])
+        logger.info(f"轨迹已从{csv_file_path}加载")
 
     def show_trace(self):
         """
